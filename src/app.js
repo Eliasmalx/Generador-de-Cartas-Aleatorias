c0f81332138276ccc796b3effba4aea95e8f02a3
Eliasmal Rodriguez <Eliasmal0228@gmail.com>
2025-03-31 11:26:40 +0000

    agregar parametros y logicas

--- src/app.js
+++ src/app.js
@@ -5,7 +5,30 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function () {
+  let generadorDecarta = () => {
+
+    let cartas = ["♦", " ♥", " ♠", "♣"]
+    let numeros = [1, 2, 3, 4, 5, 6, 7, 8] // con loops?
+  
+    let randomCarta = cartas[Math.floor(Math.random() * cartas.length)];
+    let randomNum = numeros[Math.floor(Math.random() * numeros.length)];
+  
+    let logos = document.querySelectorAll(".logo")
+    logos[0].innerHTML = randomCarta
+    logos[1].innerHTML = randomCarta
+    
+    if( logos === "♦" || logos === "♥" ){
+      logos.style.color = 'red';
+    }
+
+    let resultadoNum = document.querySelector(".numero").innerHTML = randomNum
+  
+  }
+
+  generadorDecarta()
+
+  document.getElementById("generate-btn").addEventListener("click", generadorDecarta);
 };
+
+
