b9e1eed0d89d37e5cbe218d872c6037b14ae3048
Eliasmal Rodriguez <Eliasmal0228@gmail.com>
2025-04-01 18:20:17 +0000

    arreglar temporizador , mejorar estructura

--- src/app.js
+++ src/app.js
@@ -6,31 +6,29 @@ window.onload = function () {
   let generadorDecarta = () => {
 
     let cartas = ["♦", "♥", " ♠", "♣"]
-    let numeros = [1, 2, 3, 4, 5, 6, 7, 8] // con loops?
-  
+    let numeros = [1, 2, 3, 4, 5, 6, 7, 8] 
+
     let randomCarta = cartas[Math.floor(Math.random() * cartas.length)];
     let randomNum = numeros[Math.floor(Math.random() * numeros.length)];
-  
+
     let logos = document.querySelectorAll(".logo")
     logos[0].innerHTML = randomCarta
     logos[1].innerHTML = randomCarta
-    
-    if (randomCarta === "♦" || randomCarta === "♥") {
-      logos.forEach(logo => logo.style.color = "red");
-    } else {
-      logos.forEach(logo => logo.style.color = "black");
-    }
+
+    randomCarta === "♦" || randomCarta === "♥" ?
+      logos.forEach(logo => logo.style.color = "red") : logos.forEach(logo => logo.style.color = "black");
+
 
     document.querySelector(".numero").innerHTML = randomNum
-  
+
   }
 
   generadorDecarta()
 
-  setInterval(generadorDeCarta, 10000);
 
   document.getElementById("generate-btn").addEventListener("click", generadorDecarta);
 
+  setInterval(generadorDecarta, 10000);
 };
 
 
